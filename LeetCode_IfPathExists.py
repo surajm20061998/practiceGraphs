c857c28cc14be5f2157dfc37fd0cf43b6df0bb77
surajm20061998 <surajm20061998@gmail.com>
2026-01-27 15:48:38 -0500

    Solving If Path Exists in a recursive fashion

--- LeetCode_IfPathExists.py
+++ LeetCode_IfPathExists.py
@@ -26,3 +26,39 @@ class Solution:
                     stack.append(nei_node)
         
         return False
+    
+    
+    
+#HOW TO IMPLEMENT A RECURSIVE VERSION OF THE ABOVE SOLUTION
+
+
+from collections import defaultdict
+
+class Solution:
+    
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        #convert list to adjency list
+
+        D = defaultdict(list)
+
+        for u,v in edges:
+            D[u].append(v)
+            D[v].append(u)
+        
+        seen = set()
+        seen.add(source)
+
+        def dfs(node):
+            print(node)
+            if node == destination:
+                return True
+            seen.add(node)
+            for nei in D[node]:
+                if nei in seen:
+                    continue
+                if dfs(nei):
+                    return True
+            return False
+
+        return dfs(source)
+        
